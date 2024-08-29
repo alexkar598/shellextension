19e0690fd92c832b641bea9416de719e9b821aed
alexkar598 <>
2024-08-29 19:24:20 -0400

    null byte

--- crates/shell-extension/src/utils/property.rs
+++ crates/shell-extension/src/utils/property.rs
@@ -6,7 +6,11 @@ use windows_core::PCWSTR;
 
 pub fn get_property_key_from_name(name: &str) -> windows_core::Result<PROPERTYKEY> {
     let mut key = MaybeUninit::<PROPERTYKEY>::uninit();
-    let name = OsStr::new(name).encode_wide().collect::<Vec<_>>();
+    let mut name = OsStr::new(name).encode_wide().collect::<Vec<_>>();
+    if name.last() != Some(&0) {
+        name.reserve_exact(1);
+        name.push(0)
+    }
     unsafe {
         PSGetPropertyKeyFromName(PCWSTR::from_raw(name.as_ptr()), key.as_mut_ptr())
             .map(|_| key.assume_init())
